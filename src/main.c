cd9220344a3748b88e245525781d8c6543e78995
Arnaud Taffanel <arnaud@bitcraze.se>
2016-07-26 11:19:53 +0200

    Cleaned up main from ranging code

--- src/main.c
+++ src/main.c
@@ -55,10 +55,10 @@ int initDwm1000();
 
 #define N_NODES 9
 
-uint8_t address[] = {0,0,0,0,0,0,0xcf,0xbc};
-uint8_t base_address[] = {0,0,0,0,0,0,0xcf,0xbc};
+uint8_t address[8] = {0,0,0,0,0,0,0xcf,0xbc};
+uint8_t base_address[8] = {0,0,0,0,0,0,0xcf,0xbc};
 
-float position[N_NODES][3] = {
+static float positions[][3] = {
   {0,0,0},
   {4,0,0},
   {0,5,0},
@@ -66,17 +66,13 @@ float position[N_NODES][3] = {
   {5.30f, 1.70f, 2.20f},
   {0.2f,-3.96f,2.60f},
   {4.00,  5.00,  0.00},
-}
+};
+
+float position[3];
 
 // Static system configuration
 #define MAX_ANCHORS 6
 uint8_t anchors[MAX_ANCHORS];
-
-// The four packets for ranging
-#define POLL 0x01   // Poll is initiated by the tag
-#define ANSWER 0x02
-#define FINAL 0x03
-#define REPORT 0x04 // Report contains all measurement from the anchor
 
 typedef struct {
   uint8_t pollRx[5];
@@ -102,36 +98,11 @@ typedef union timestamp_u {
   } __attribute__((packed));
 } timestamp_t;
 
-// Timestamps for ranging
-dwTime_t poll_tx;
-dwTime_t poll_rx;
-dwTime_t answer_tx;
-dwTime_t answer_rx;
-dwTime_t final_tx;
-dwTime_t final_rx;
-
-uint32_t rangingTick;
-
-float pressure, temperature, asl;
-bool pressure_ok;
-
 const double C = 299792458.0;       // Speed of light
 const double tsfreq = 499.2e6 * 128;  // Timestamp counter frequency
 
 #define ANTENNA_OFFSET 154.6   // In meter
 #define ANTENNA_DELAY  (ANTENNA_OFFSET*499.2e6*128)/299792458.0 // In radio tick
-
-static packet_t rxPacket;
-static packet_t txPacket;
-static volatile uint8_t curr_seq = 0;
-
-// Sniffer queue
-#define QUEUE_LEN 16
-static packet_t snifferPacketQueue[QUEUE_LEN];
-static int snifferPacketLength[QUEUE_LEN];
-static int queue_head = 0;
-static int queue_tail = 0;
-static volatile uint32_t dropped = 0;
 
 static void restConfig();
 static void changeAddress(uint8_t addr);
@@ -140,198 +111,24 @@ static void changeMode(CfgMode newMode);
 static void printMode();
 static void help();
 
+static bool receivedFlag = false;
+static bool receiveFailedFlag = false;
+static bool transmitedFlag = false;
+
 // #define printf(...)
 #define debug(...) // printf(__VA_ARGS__)
 
 void txcallback(dwDevice_t *dev)
 {
-  dwTime_t departure;
-  dwGetTransmitTimestamp(dev, &departure);
-
-  debug("TXCallback: ");
-
-  switch (txPacket.payload[0]) {
-    case POLL:
-      rangingTick = HAL_GetTick();
-      debug("POLL\r\n");
-      poll_tx = departure;
-      break;
-    case ANSWER:
-    debug("ANSWER to %02x at %04x\r\n", txPacket.destAddress[0], (unsigned int)departure.low32);
-      answer_tx = departure;
-      break;
-    case FINAL:
-      debug("FINAL\r\n");
-      final_tx = departure;
-      break;
-    case REPORT:
-      debug("REPORT\r\n");
-      break;
-  }
+  transmitedFlag = true;
 }
 
-#define TYPE 0
-#define SEQ 1
-
 void rxcallback(dwDevice_t *dev) {
-  dwTime_t arival;
-  int dataLength = dwGetDataLength(dev);
+  receivedFlag = true;
+}
 
-  if (dataLength == 0) return;
-
-  bzero(&rxPacket, MAC802154_HEADER_LENGTH);
-
-  debug("RXCallback(%d): ", dataLength);
-
-  if (mode == modeSniffer) {
-    if ((queue_head+1)%QUEUE_LEN == queue_tail) {
-      dropped++;
-      dwNewReceive(dev);
-      dwSetDefaults(dev);
-      dwStartReceive(dev);
-      return;
-    }
-
-    // Queue the received packet, the main loop will print it on the console
-    dwGetData(dev, (uint8_t*)&snifferPacketQueue[queue_head], dataLength);
-    snifferPacketLength[queue_head] = dataLength;
-    queue_head = (queue_head+1)%QUEUE_LEN;
-    dwNewReceive(dev);
-    dwSetDefaults(dev);
-    dwStartReceive(dev);
-    return;
-  }
-
-  dwGetData(dev, (uint8_t*)&rxPacket, dataLength);
-
-  if (memcmp(rxPacket.destAddress, address, 8)) {
-    debug("Not for me! for %02x with %02x\r\n", rxPacket.destAddress[0], rxPacket.payload[0]);
-    dwNewReceive(dev);
-    dwSetDefaults(dev);
-    dwStartReceive(dev);
-    return;
-  }
-
-
-
-  memcpy(txPacket.destAddress, rxPacket.sourceAddress, 8);
-  memcpy(txPacket.sourceAddress, rxPacket.destAddress, 8);
-
-  switch(rxPacket.payload[TYPE]) {
-    // Anchor received messages
-    case POLL:
-      debug("POLL from %02x at %04x\r\n", rxPacket.sourceAddress[0], (unsigned int)arival.low32);
-      rangingTick = HAL_GetTick();
-
-      txPacket.payload[TYPE] = ANSWER;
-      txPacket.payload[SEQ] = rxPacket.payload[SEQ];
-
-      dwNewTransmit(dev);
-      dwSetDefaults(dev);
-      dwSetData(dev, (uint8_t*)&txPacket, MAC802154_HEADER_LENGTH+2);
-
-      dwWaitForResponse(dev, true);
-      dwStartTransmit(dev);
-
-      dwGetReceiveTimestamp(dev, &arival);
-      poll_rx = arival;
-      break;
-    case FINAL:
-    {
-      reportPayload_t *report = (reportPayload_t *)(txPacket.payload+2);
-
-      debug("FINAL\r\n");
-
-      dwGetReceiveTimestamp(dev, &arival);
-      final_rx = arival;
-
-      txPacket.payload[TYPE] = REPORT;
-      txPacket.payload[SEQ] = rxPacket.payload[SEQ];
-      memcpy(&report->pollRx, &poll_rx, 5);
-      memcpy(&report->answerTx, &answer_tx, 5);
-      memcpy(&report->finalRx, &final_rx, 5);
-      report->pressure = pressure;
-      report->temperature = temperature;
-      report->asl = asl;
-      report->pressure_ok = pressure_ok;
-
-      dwNewTransmit(dev);
-      dwSetDefaults(dev);
-      dwSetData(dev, (uint8_t*)&txPacket, MAC802154_HEADER_LENGTH+2+sizeof(reportPayload_t));
-
-      dwWaitForResponse(dev, true);
-      dwStartTransmit(dev);
-
-      break;
-    }
-    // Tag received messages
-    case ANSWER:
-      debug("ANSWER\r\n");
-
-      if (rxPacket.payload[SEQ] != curr_seq) {
-        debug("Wrong sequence number!\r\n");
-        return;
-      }
-
-      txPacket.payload[0] = FINAL;
-      txPacket.payload[SEQ] = rxPacket.payload[SEQ];
-
-      dwNewTransmit(dev);
-      dwSetData(dev, (uint8_t*)&txPacket, MAC802154_HEADER_LENGTH+2);
-
-      dwWaitForResponse(dev, true);
-      dwStartTransmit(dev);
-
-      dwGetReceiveTimestamp(dev, &arival);
-      answer_rx = arival;
-      break;
-    case REPORT:
-    {
-      reportPayload_t *report = (reportPayload_t *)(rxPacket.payload+2);
-      double tround1, treply1, treply2, tround2, tprop_ctn, tprop, distance;
-
-      debug("REPORT\r\n");
-
-      if (rxPacket.payload[SEQ] != curr_seq) {
-        printf("Wrong sequence number!\r\n");
-        return;
-      }
-
-      memcpy(&poll_rx, &report->pollRx, 5);
-      memcpy(&answer_tx, &report->answerTx, 5);
-      memcpy(&final_rx, &report->finalRx, 5);
-
-      printf("%02x%08x ", (unsigned int)poll_tx.high8, (unsigned int)poll_tx.low32);
-      printf("%02x%08x\r\n", (unsigned int)poll_rx.high8, (unsigned int)poll_rx.low32);
-      printf("%02x%08x ", (unsigned int)answer_tx.high8, (unsigned int)answer_tx.low32);
-      printf("%02x%08x\r\n", (unsigned int)answer_rx.high8, (unsigned int)answer_rx.low32);
-      printf("%02x%08x ", (unsigned int)final_tx.high8, (unsigned int)final_tx.low32);
-      printf("%02x%08x\r\n", (unsigned int)final_rx.high8, (unsigned int)final_rx.low32);
-
-      tround1 = answer_rx.low32 - poll_tx.low32;
-      treply1 = answer_tx.low32 - poll_rx.low32;
-      tround2 = final_rx.low32 - answer_tx.low32;
-      treply2 = final_tx.low32 - answer_rx.low32;
-
-      printf("%08x %08x\r\n", (unsigned int)tround1, (unsigned int)treply2);
-      printf("\\    /   /     \\\r\n");
-      printf("%08x %08x\r\n", (unsigned int)treply1, (unsigned int)tround2);
-
-      tprop_ctn = ((tround1*tround2) - (treply1*treply2)) / (tround1 + tround2 + treply1 + treply2);
-
-      printf("TProp (ctn): %d\r\n", (unsigned int)tprop_ctn);
-
-      tprop = tprop_ctn/tsfreq;
-      distance = C * tprop;
-
-      printf("distance %d: %5dmm\r\n", rxPacket.sourceAddress[0], (unsigned int)(distance*1000));
-
-      dwGetReceiveTimestamp(dev, &arival);
-      printf("Total in-air time (ctn): 0x%08x\r\n", (unsigned int)(arival.low32-poll_tx.low32));
-
-      break;
-    }
-  }
+void rxFailedCallback(dwDevice_t *dev) {
+  receiveFailedFlag = true;
 }
 
 bool contains(int* list, int length, int value)
@@ -345,6 +142,10 @@ bool contains(int* list, int length, int value)
   }
 
   return false;
+}
+
+void stateMachineStep(dwDevice_t *uwb, bool receivedFlag, bool receiveFailedFlag, bool transmitedFlag) {
+  ;
 }
 
 dwDevice_t dwm_device;
@@ -425,6 +226,10 @@ int main() {
 
   if (cfgReadU8(cfgAddress, &address[0])) {
     printf("CONFIG\t: Address is 0x%X\r\n", address[0]);
+    position[0] = positions[address[0]][0];
+    position[1] = positions[address[0]][1];
+    position[2] = positions[address[0]][2];
+    printf("CONFIG\t: Position: x: %f, y: %f, z: %f", position[0], position[1], position[2]);
   } else {
     printf("CONFIG\t: Address not found!\r\n");
   }
@@ -465,6 +270,7 @@ int main() {
 
   dwAttachSentHandler(dwm, txcallback);
   dwAttachReceivedHandler(dwm, rxcallback);
+  dwAttachReceiveFailedHandler(dwm, rxFailedCallback);
 
   dwNewConfiguration(dwm);
   dwSetDefaults(dwm);
@@ -480,8 +286,8 @@ int main() {
   usbcommSetSystemStarted(true);
 
   // Initialize the packet in the TX buffer
-  MAC80215_PACKET_INIT(txPacket, MAC802154_TYPE_DATA);
-  txPacket.pan = 0xbccf;
+  // MAC80215_PACKET_INIT(txPacket, MAC802154_TYPE_DATA);
+  // txPacket.pan = 0xbccf;
 
   if (mode == modeAnchor || mode == modeSniffer) {
     dwNewReceive(dwm);
@@ -496,16 +302,6 @@ int main() {
   while(1) {
     usbcommTick();
 
-    // Measure pressure
-    if (mode != modeSniffer) {
-      if(lps25hGetData(&pressure, &temperature, &asl)) {
-        pressure_ok = true;
-      } else {
-        printf("Fail reading pressure\r\n");
-        printf("pressure not ok\r\n");
-      }
-    }
-
     // Accepts serial commands
 #ifdef USE_FTDI_UART
     if (HAL_UART_Receive(&huart1, (uint8_t*)&ch, 1, 0) == HAL_OK) {
@@ -515,59 +311,13 @@ int main() {
       handleInput(ch);
     }
 
-    if (mode == modeSniffer) {
-      static uint32_t prevDropped = 0;
-
-      if (dropped != prevDropped) {
-        printf("Dropped!\r\n");
-        prevDropped = dropped;
-      }
-
-      if (queue_tail != queue_head) {
-        printf("From %02x to %02x data ", snifferPacketQueue[queue_tail].sourceAddress[0],
-                                          snifferPacketQueue[queue_tail].destAddress[0]);
-        for (int i=0; i<(snifferPacketLength[queue_tail] - MAC802154_HEADER_LENGTH); i++) {
-          printf("0x%02x ", snifferPacketQueue[queue_tail].payload[i]);
-        }
-        queue_tail = (queue_tail+1)%QUEUE_LEN;
-        printf("\r\n");
-      }
-    }
-
-    if (mode == modeTag) {
-       for (i=0; i<anchorListSize; i++) {
-         printf ("Interrogating anchor %d\r\n", anchors[i]);
-         base_address[0] = anchors[i];
-         dwIdle(dwm);
-
-         txPacket.payload[TYPE] = POLL;
-         txPacket.payload[SEQ] = ++curr_seq;
-
-         memcpy(txPacket.sourceAddress, address, 8);
-         memcpy(txPacket.destAddress, base_address, 8);
-
-         dwNewTransmit(dwm);
-         dwSetDefaults(dwm);
-         dwSetData(dwm, (uint8_t*)&txPacket, MAC802154_HEADER_LENGTH+2);
-
-         dwWaitForResponse(dwm, true);
-         dwStartTransmit(dwm);
-
-         HAL_Delay(30);
-       }
-     }
+    stateMachineStep(dwm, receivedFlag, receiveFailedFlag, transmitedFlag);
 
     // Handling of the LEDs
     if (HAL_GetTick() > (ledTick+250)) {
       ledTick = HAL_GetTick();
       ledState = !ledState;
     }
-
-     if ((HAL_GetTick() < (rangingTick+50)) && ledState) {
-       ledOn(ledRanging);
-     } else {
-       ledOff(ledRanging);
-     }
 
      ledOff(ledSync);
 
